e197613c12a825a6bcab911d8bde65f0410364ce
sowjanya2098 <bandakavi20@gmail.com>
2026-06-30 21:11:26 +0530

    Update notification.py

--- backend/lambda/submit_leave/notification.py
+++ backend/lambda/submit_leave/notification.py
@@ -1,14 +1,19 @@
+import json
 import boto3
 
 sns = boto3.client('sns')
 
-TOPIC_ARN = "arn:aws:sns:us-east-1:364042450737:Leavenotification:adde33f7-2734-430a-b886-875f7ddbcfca"
+# Replace with your SNS Topic ARN
+TOPIC_ARN = "arn:aws:sns:us-east-1:364042450737:Leavenotififcation"
 
 def lambda_handler(event, context):
 
-    employee_name = event['employee_name']
-    leave_type = event['leave_type']
-    status = event['status']
+    employee_name = event.get("employee_name", "Employee")
+    employee_email = event.get("employee_email", "employee@example.com")
+    leave_type = event.get("leave_type", "Leave")
+    status = event.get("status", "Approved")
+
+    subject = f"Leave Request {status}"
 
     message = f"""
 Hello {employee_name},
@@ -17,54 +22,19 @@ Your leave request has been {status}.
 
 Leave Type: {leave_type}
 
-Regards,
-HR Team
-"""
-
-    sns.publish(
-        TopicArn=arn:aws:sns:us-east-1:364042450737:Leavenotification:adde33f7-2734-430a-b886-875f7ddbcfca,
-        Subject="Leave Request Status",
-        Message=message
-    )
-
-    return {
-        "statusCode": 200,
-        "body": "Notification Sent Successfully"
-    }
-
-    unzipimport boto3
-
-sns = boto3.client('sns')
-
-TOPIC_ARN = "arn:aws:sns:us-east-1:364042450737:Leavenotification:adde33f7-2734-430a-b886-875f7ddbcfca"
-
-def lambda_handler(event, context):
-
-    employee_name = event['employee_name']
-    leave_type = event['leave_type']
-    status = event['status']
-
-    message = f"""
-Hello {employee_name},
-
-Your leave request has been {status}.
-
-Leave Type: {leave_type}
+Status: {status}
 
 Regards,
 HR Team
 """
 
     sns.publish(
-        TopicArn=arn:aws:sns:us-east-1:364042450737:Leavenotification:adde33f7-2734-430a-b886-875f7ddbcfca,
-        Subject="Leave Request Status",
+        TopicArn=TOPIC_ARN,
+        Subject=subject,
         Message=message
     )
 
     return {
         "statusCode": 200,
-        "body": "Notification Sent Successfully"
+        "body": json.dumps("Notification Sent Successfully")
     }
-
-    unzip 
-    
